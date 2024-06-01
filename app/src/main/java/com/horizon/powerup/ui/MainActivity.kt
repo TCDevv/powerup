733deff74ceb82395de851d06a45a9a3a2f54706
Phát La (Sr. Software Engineer) <phat.la@be.com.vn>
2024-06-01 19:28:58 +0700

    Remove Unused

--- app/src/main/java/com/horizon/powerup/ui/MainActivity.kt
+++ app/src/main/java/com/horizon/powerup/ui/MainActivity.kt
@@ -1,20 +1,11 @@
 package com.horizon.powerup.ui
 
 import android.os.Bundle
-import androidx.activity.result.ActivityResultLauncher
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.FragmentManager
 import com.horizon.powerup.R
 
 class MainActivity : AppCompatActivity(), Navigator.Container {
-    private companion object {
-        private const val REQUEST_IGNORE_BATTERY_OPTIMIZATION = 1
-    }
-
-    private var onRecentBackPressedTime: Long = 0
-
-    private var permissionLauncher: ActivityResultLauncher<Int>? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
